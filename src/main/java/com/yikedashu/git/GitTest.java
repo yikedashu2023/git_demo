7b906213e444fb7db12782f35bfa662ed0cb2651
yikedashu <yikedashu66@qq.com>
2023-05-14 14:19:52 +0800

    th_commit

--- src/main/java/com/yikedashu/git/GitTest.java
+++ src/main/java/com/yikedashu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("你好Git");
         System.out.println("你好Git001");
+        System.out.println("你好Git002");
     }
 }
