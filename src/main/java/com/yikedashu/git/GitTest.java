c8c0f605941d76aea6bb8f60234b159cd805329e
yikedashu <yikedashu66@qq.com>
2023-05-14 14:05:17 +0800

    frist_commit

---
+++ src/main/java/com/yikedashu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.yikedashu.git;
+
+/**
+ * @author yikedashu
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("你好Git");
+    }
+}
